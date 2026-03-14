371175f177c6cfd297a0db81a7ce92c58c760daf
Rasini123 <rasinikawya56@gmail.com>
2026-03-14 20:19:13 +0530

    Fixed the side bar

--- Public/Supplier/profile.php
+++ Public/Supplier/profile.php
@@ -65,14 +65,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link rel="shortcut icon" href="<?= $sbase ?>static/img/9.png?v=<?= time(); ?>" />
 
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css" crossorigin="anonymous" />
 
   <title>eSupplier-CDPLC</title>
 
   <link href="<?= $sbase ?>static/css/app.css?v=<?= time(); ?>" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+  <style>
+    .sidebar {
+      position: sticky !important;
+      top: 0;
+      height: 100vh;
+      z-index: 1000;
+    }
+  </style>
+
 
 </head>
 
@@ -823,9 +833,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <!-- timer script sessionUnset -->
   <script src="<?= $sbase ?>js/sessionUnset.js?v=<?= time(); ?>"></script>
 
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
   <script src="<?= $sbase ?>js/translate.js?v=<?= time(); ?>"></script>
+
 </body>
 
 </html>
